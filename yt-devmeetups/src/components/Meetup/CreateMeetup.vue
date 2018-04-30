d094c55e5f7c8f1dbc7e5c81554c0efb657076f3
zkolomiiets <zkolomiiets@acceptic.net>
2018-04-30 19:34:41 +0300

    Meetups project

---
+++ yt-devmeetups/src/components/Meetup/CreateMeetup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>The create meetups page</p>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'CreateMeetup'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ yt-devmeetups/src/components/User/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>The sign in page</p>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'SignUp'
+    }
+</script>
+
+<style scoped>
+
+</style>
