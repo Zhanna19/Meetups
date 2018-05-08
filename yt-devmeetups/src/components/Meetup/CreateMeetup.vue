fd116915dcf275eaf633b0ab63ea62c0162d02de
zkolomiiets <zkolomiiets@acceptic.net>
2018-05-08 14:58:51 +0300

    Form

--- yt-devmeetups/src/components/Meetup/CreateMeetup.vue
+++ yt-devmeetups/src/components/Meetup/CreateMeetup.vue
@@ -1,12 +1,88 @@
 <template>
-  <div>
-    <p>The create meetups page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex x12 offset-sm2>
+        <h2 class="primary--text">Create a new meetup</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex x12>
+        <v-form @submit.prevent="onCreateMeetup">
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm2>
+              <v-text-field
+                v-model="title"
+                label="Title"
+                required
+              >
+              </v-text-field>
+              <v-text-field
+                v-model="location"
+                label="Location"
+                required
+              >
+              </v-text-field>
+              <v-text-field
+                v-model="imageUrl"
+                label="Image URL"
+                required
+              >
+              </v-text-field>
+              <v-flex x12 sm6>
+                <img :src="imageUrl" height="150">
+              </v-flex>
+              <v-text-field
+                v-model="description"
+                label="Description"
+                multi-line
+                required
+              >
+              </v-text-field>
+              <v-btn
+                class="primary"
+                :disabled="!formValid"
+                type="submit"
+              >
+                CREATE MEETUP
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
     export default {
-      name: 'CreateMeetup'
+      name: 'CreateMeetup',
+      data () {
+        return {
+          title: '',
+          location: '',
+          imageUrl: '',
+          description: ''
+        }
+      },
+      computed: {
+        formValid () {
+          return this.title !== '' &&
+            this.location !== '' &&
+            this.imageUrl !== '' &&
+            this.description !== ''
+        }
+      },
+      methods: {
+        onCreateMeetup () {
+          let meetupData = {
+            title: this.title,
+            location: this.location,
+            imageUrl: this.imageUrl,
+            description: this.description
+          }
+          this.$store.dispatch('createMeetup', meetupData)
+        }
+      }
     }
 </script>
 
--- yt-devmeetups/src/store/index.js
+++ yt-devmeetups/src/store/index.js
@@ -27,8 +27,23 @@ export const store = new Vuex.Store({
       registerMeetups: ['dffdgfdgd']
     }
   },
-  mutations: {},
-  action: {},
+  action: {
+    createMeetup ({ commit }, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description
+      }
+      // Reach out to firebase and store it
+      commit('createMeetup', meetup)
+    }
+  },
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
   getters: {
     loadedMeetups (state) {
       return state.loadedMeetups.sort((meetupA, meetupB) => {
